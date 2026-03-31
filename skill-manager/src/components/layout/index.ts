e3d70b9ef840204e49bd8139b07c2b182000b592
hanruochong <asong6824@outlook.com>
2026-03-31 10:42:26 +0800

    chore: remove duplicate skill-manager directory from git index

--- skill-manager/src/components/layout/index.ts
+++
@@ -1,3 +0,0 @@
-export { TitleBar } from './TitleBar';
-export { StatusBar } from './StatusBar';
-export { Sidebar } from './Sidebar';
--- skill-manager/src/components/ui/index.ts
+++
@@ -1,4 +0,0 @@
-export { Button } from './Button';
-export { SegmentedControl } from './SegmentedControl';
-export { Chip } from './Chip';
-export { Input } from './Input';
